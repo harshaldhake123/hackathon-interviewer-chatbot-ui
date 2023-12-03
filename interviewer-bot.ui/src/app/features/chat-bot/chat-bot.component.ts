1a25d8718286759eacfbf06c7c75849643c46ad4
Harshal Dhake <harshaldhake123@gmail.com>
2023-12-03 23:33:12 +0530

    refactor into message component

--- interviewer-bot.ui/src/app/features/chat-bot/chat-bot.component.ts
+++ interviewer-bot.ui/src/app/features/chat-bot/chat-bot.component.ts
@@ -1,27 +1,25 @@
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder } from '@angular/forms';
 import { UserChatInputComponent } from '../user-chat-input/user-chat-input.component';
+import { ChatMessageComponent } from 'src/app/features/chat-message/chat-message.component';
 import {
   BotMessagesService,
   Message,
 } from 'src/app/services/bot-messages.service';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 @UntilDestroy()
 @Component({
   selector: 'app-chat-bot',
   standalone: true,
-  imports: [CommonModule, UserChatInputComponent],
+  imports: [CommonModule, UserChatInputComponent, ChatMessageComponent],
   templateUrl: './chat-bot.component.html',
   styleUrls: ['./chat-bot.component.scss'],
 })
 export class ChatBotComponent implements OnInit {
   public chatMessages: Message[] = [];
 
-  constructor(
-    private botMessageService: BotMessagesService,
-  ) {}
+  constructor(private botMessageService: BotMessagesService) {}
 
   public ngOnInit(): void {
     this.botMessageService
--- interviewer-bot.ui/src/app/features/chat-message/chat-message.component.ts
+++ interviewer-bot.ui/src/app/features/chat-message/chat-message.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Message } from 'src/app/services/bot-messages.service';
 
 @Component({
   selector: 'app-chat-message',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './chat-message.component.html',
-  styleUrls: ['./chat-message.component.scss']
+  styleUrls: ['./chat-message.component.scss'],
 })
 export class ChatMessageComponent {
-
+  @Input() public message!: Message;
 }
